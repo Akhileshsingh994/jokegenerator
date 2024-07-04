91d1dadcac9c4c23259305940dc27c31a51ad4f2
Akhilesh singh <103052539+Akhileshsingh994@users.noreply.github.com>
2024-07-04 16:43:58 +0530

    created a basic app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
+import Joke from './components/Joke';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header>
+                <h1>SMILING BINGO</h1>
+            </header>
+            <main>
+                <Joke />
+            </main>
+            <footer>
+                <p>Have a great day with some laughs!</p>
+            </footer>
+        </div>
+    );
 }
 
 export default App;
